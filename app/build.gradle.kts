2a041c1e46e71658b35ade60a2fd06f31481f3fa
sheldonrocks2022-cmyk <sheldonrocks2022@gmail.com>
2026-09-17 18:52:09 -0500

    Version JARVIS for unique test builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.esn.jarvis"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.2.0"
+        versionCode = 133
+        versionName = "0.3.133"
     }
 
     buildTypes {
